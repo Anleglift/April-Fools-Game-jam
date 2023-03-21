6311a0d05b5375eeea6788d3c42439f2fb6e74f0
Anleglift <126957403+Anleglift@users.noreply.github.com>
2023-03-21 18:18:14 +0200

    Added gun and shooting

---
+++ AP/Assets/Scripts/New Folder/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    
+    private void Start()
+    {
+      
+       transform.rotation = Camera.main.transform.rotation;
+       rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = transform.TransformDirection(Vector3.forward);
+        rb.velocity = moveDirection * speed;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("sizeable"))
+            Destroy(gameObject);
+        else
+            Destroy(gameObject);
+    }
+    
+}
---
+++ AP/Assets/Scripts/New Folder/Weapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Vector3 localPosition;
+    public GameObject bullet;
+    public bool CanShoot;
+    private float delay = 2f;
+    private void Start()
+    {
+         CanShoot = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = localPosition;
+        if (Input.GetMouseButtonDown(0) && CanShoot)
+        {
+            Instantiate(bullet, transform.position, Camera.main.transform.rotation);
+            CanShoot=false;  
+            Invoke("ResetShoot",delay);
+        }
+    }
+    private void ResetShoot()
+    {
+        CanShoot = true;
+    }
+}
