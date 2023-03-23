23065e16e39e54bdfa13164d1d025fc2acabb371
Anleglift <126957403+Anleglift@users.noreply.github.com>
2023-03-23 23:59:44 +0200

    Fixed platforms

--- AP/Assets/Scripts/Lever/MoveOnWaypoint.cs
+++ AP/Assets/Scripts/Lever/MoveOnWaypoint.cs
@@ -4,36 +4,61 @@ using UnityEngine;
 
 public class MoveOnWaypoint : MonoBehaviour
 {
+    public GameObject[] waypoints;
     public leveraction leveraction;
-    public List<GameObject> waypoints;
-    public float speed = 5;
-    int index = 0;
-    public bool loop = true;
+    public float speed = 5f;
+    public float smoothTime = 0.5f;
 
+    private Rigidbody rb;
+    private int currentWaypointIndex = 0;
+    private int waypointCount;
+    private Vector3 velocity = Vector3.zero;
 
-    void Update()
+    private void Start()
+    {
+        transform.position = waypoints[currentWaypointIndex].transform.position;
+        rb = GetComponent<Rigidbody>();
+        waypointCount = waypoints.Length;
+    }
+
+    private void FixedUpdate()
     {
         if (leveraction.On)
         {
-            Vector3 destination = waypoints[index].transform.position;
-            Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
-            transform.position = newPos;
+            Vector3 destination = waypoints[currentWaypointIndex + 1].transform.position;
+            Vector3 direction = (destination - transform.position).normalized;
+            float distanceToDestination = Vector3.Distance(transform.position, destination);
 
-            float distance = Vector3.Distance(transform.position, destination);
-            if (distance <= 0.05)
+            if (distanceToDestination > 0.1f)
             {
-                if (index < waypoints.Count - 1)
-                    index++;
-                else
+                float targetSpeed = Mathf.Lerp(0, speed, distanceToDestination / 2);
+                rb.MovePosition(transform.position + direction * targetSpeed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                currentWaypointIndex++;
+                if (currentWaypointIndex == waypointCount - 1)
                 {
-                    if (loop)
-                    {
-                        index = 0;
-                    }
-
+                    currentWaypointIndex = -1;
                 }
             }
         }
+    }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
     }
 }
