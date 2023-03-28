2793dc9ac348c9f9e254ce1265c14ab041399d38
Anleglift <126957403+Anleglift@users.noreply.github.com>
2023-03-29 00:28:28 +0300

    Added Stairs,outline,textures,organised everything

--- AP/Assets/Scripts/FirstPerson/PlayerMovement.cs
+++ AP/Assets/Scripts/FirstPerson/PlayerMovement.cs
@@ -37,8 +37,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
-        readyToJump = true; 
+        readyToJump = true;
     }
 
     private void Update()
@@ -64,7 +63,7 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
 
@@ -80,15 +79,15 @@ public class PlayerMovement : MonoBehaviour
 
         if (grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        else if(!grounded)
+        else if (!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
     }
 
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f ,rb.velocity.z);
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        if(flatVel.magnitude > moveSpeed)
+        if (flatVel.magnitude > moveSpeed)
         {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
@@ -106,4 +105,5 @@ public class PlayerMovement : MonoBehaviour
     {
         readyToJump = true;
     }
+   
 }
